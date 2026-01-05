00000010f5482613000cd224afdb40b84b4f25ef
MCSeekeri <mcseekeri@outlook.com>
2026-01-05 14:32:12 +0800

    删去不必要的文件

--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -1,13 +1,6 @@
 import { siteConfig } from "../config";
 import type I18nKey from "./i18nKey";
 import { en } from "./languages/en";
-import { es } from "./languages/es";
-import { id } from "./languages/id";
-import { ja } from "./languages/ja";
-import { ko } from "./languages/ko";
-import { th } from "./languages/th";
-import { tr } from "./languages/tr";
-import { vi } from "./languages/vi";
 import { zh_CN } from "./languages/zh_CN";
 import { zh_TW } from "./languages/zh_TW";
 
@@ -18,24 +11,12 @@ export type Translation = {
 const defaultTranslation = en;
 
 const map: { [key: string]: Translation } = {
-	es: es,
 	en: en,
 	en_us: en,
 	en_gb: en,
 	en_au: en,
 	zh_cn: zh_CN,
 	zh_tw: zh_TW,
-	ja: ja,
-	ja_jp: ja,
-	ko: ko,
-	ko_kr: ko,
-	th: th,
-	th_th: th,
-	vi: vi,
-	vi_vn: vi,
-	id: id,
-	tr: tr,
-	tr_tr: tr,
 };
 
 export function getTranslation(lang: string): Translation {
